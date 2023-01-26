e9609d53278f89e85e458394f26b71cd4097a8bc
dancunrandy <dancunrandy@gmail.com>
2023-01-27 02:36:13 +0300

    handle submit form

--- src/Components/Charities.js
+++ src/Components/Charities.js
@@ -26,4 +26,25 @@ export default function Charities() {
           Accept: "application/json",
           "Access-Control-Allow-Origin": "http://localhost:3000",
         },
-      });
+      }); // TODO: use reducer to handle all shared state
+      setAllOrganisations(response.data.organizations.organization);
+      setIsFetched(true);
+      if (response.data.organizations.hasNext) {
+        setHasNext(true);
+        setNextOrgId(response.data.organizations.nextOrgId);
+      }
+    } catch (error) {
+      if (error.response) {
+        console.log(error.response.status);
+      }
+    }
+  }, [allOrganisationsUrl.href]);
+  const handleSubmitForm = (event) => {
+    event.preventDefault();
+    let tempLink;
+    if (organisationCategory) {
+      tempLink = new URL(`${ORGANISATIONS_ENDPOINT}${organisationCategory}`);
+      tempLink.searchParams.set("api_key", process.env.REACT_APP_API_KEY);
+    }
+    setAllOrganisationsUrl(tempLink);
+  };
