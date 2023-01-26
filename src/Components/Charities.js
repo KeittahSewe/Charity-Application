6b850ce8f9208627c545883bf0a19f6f3e2c9acb
dancunrandy <dancunrandy@gmail.com>
2023-01-27 02:33:35 +0300

    fetching data

--- src/Components/Charities.js
+++ src/Components/Charities.js
@@ -7,4 +7,23 @@ const ORGANISATIONS_ENDPOINT = new URL(
 export default function Charities() {
   const [allOrganisationsUrl, setAllOrganisationsUrl] = useState(
     ORGANISATIONS_ENDPOINT
-  );
+  ); const [allOrganisations, setAllOrganisations] = useState("");
+  // TODO: Use reducer to handle "comorbid" states
+  const [isFetched, setIsFetched] = useState(false);
+  const [organisationLocation, setOrganisationLocation] = useState("");
+  const [organisationCategory, setOrganisationCategory] = useState("active");
+  const [hasNext, setHasNext] = useState(false);
+  const [nextOrgId, setNextOrgId] = useState(null);
+  const fetchAllOrganisations = useCallback(async () => {
+    allOrganisationsUrl.searchParams.set(
+      "api_key",
+      process.env.REACT_APP_API_KEY
+    );
+    try {
+      const response = await axios.get(allOrganisationsUrl, {
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+          "Access-Control-Allow-Origin": "http://localhost:3000",
+        },
+      });
