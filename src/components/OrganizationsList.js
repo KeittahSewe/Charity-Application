35c1ba944c037a864f5faf715191ce18aa9e4ed9
LouisOkwaro <okwarolouis@gmail.comgit config --global user.email okwarolouis@gmail.com>
2023-01-27 04:01:00 +0300

    creation of new URL by concatenating the url prop with the id property

--- src/components/OrganizationsList.js
+++ src/components/OrganizationsList.js
@@ -13,3 +13,18 @@ export default function OrganizationsList({
     );
   });
 }
+const Organization = ({ organization, url }) => {
+    const link = new URL(`${url}${organization.id}`);
+    link.searchParams.set("api_key", process.env.REACT_APP_API_KEY);
+    return (
+      <div key={organization.id}>
+         <img src={organization.logoUrl} alt="random"/>
+        <span style={{ fontWeight: "bold" }}>
+          <a href={link.href}>{organization.name}</a>
+        </span>
+        <span>{organization.country}</span>
+        <span>{organization.city}</span>
+        <span>{organization.mission}</span>
+      </div>
+    );
+  };
